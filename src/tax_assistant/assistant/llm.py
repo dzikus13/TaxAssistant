a48eeb9506c8f0c6e35e19c613f5b43945094f3e
Piotr Kryska <piotr.kryska@yarrl.pl>
2024-09-29 00:25:17 +0200

    prompt changes

--- src/tax_assistant/assistant/llm.py
+++ src/tax_assistant/assistant/llm.py
@@ -1,6 +1,7 @@
 from openai import OpenAI
-import os
+from openai.types.chat.completion_create_params import ResponseFormat
 
+import os
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
@@ -15,7 +16,7 @@ class OpenAIConnector:
         self.LLM_MODEL = "gpt-4o-2024-08-06"
         self.temperature = 0
         self.top_p = 0
-        self.system_prompt
+        self.system_prompt = system_prompt
 
     def build_messages(self, user_prompt, message_history):
         messages = []
@@ -35,21 +36,22 @@ class OpenAIConnector:
 
         messages.append({
                     "role": "user",
-                    "cpmtemt": user_prompt    
+                    "content": user_prompt    
                 })
         return messages
 
     def get_gpt_response(self, user_prompt, history):
         messages = self.build_messages(user_prompt, history)
+        print(messages)
         response = self.client.chat.completions.create(
             model=self.LLM_MODEL,
             messages=messages,
             temperature=self.temperature,
             top_p=self.top_p,
-            response_format="json_object"
+            response_format={"type": "json_object"}   #r esponse_format
         )
-
-        return response
+        message = response.choices[0].message.content
+        return message
     
     def discover_topic(self, prompt):
         return "pcc-3"
