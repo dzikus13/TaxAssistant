3b769f73aa1a7a07c396204ed1d70aefc56bac17
Piotr Kryska <piotr.kryska@yarrl.pl>
2024-09-29 11:59:12 +0200

    statics

--- src/tax_assistant/scripts/clean_url.py
+++ src/tax_assistant/scripts/clean_url.py
@@ -5,14 +5,14 @@ assets_dir = "assistant/static/assets/"
 
 
 def main(api_url):
-    for filename in glob.glob(assets_dir+"/*.css"):
+    for filename in glob.glob(assets_dir+"/index-*.css"):
         with open(filename) as f:
             data = f.read()
             data = data.replace("192.168.0.11:8000", api_url)
             with open(assets_dir+"index.css", "w") as w:
                 w.write(data)
 
-    for filename in glob.glob(assets_dir+"/*.js"):
+    for filename in glob.glob(assets_dir+"/index-*.js"):
         with open(filename) as f:
             data = f.read()
             data = data.replace("192.168.0.11:8000", api_url)
