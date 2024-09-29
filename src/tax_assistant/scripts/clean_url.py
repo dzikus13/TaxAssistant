115a7cf691b98981547fcdc12ab9465c509e5052
Piotr Kryska <piotr.kryska@yarrl.pl>
2024-09-29 10:08:29 +0200

    UA

---
+++ src/tax_assistant/scripts/clean_url.py
@@ -0,0 +1,25 @@
+import sys
+import glob
+
+assets_dir = "assistant/static/assets/"
+
+
+def main(api_url):
+    for filename in glob.glob(assets_dir+"/*.css"):
+        with open(filename) as f:
+            data = f.read()
+            data = data.replace("192.168.0.11", api_url)
+            with open(assets_dir+"index.css", "w") as w:
+                w.write(data)
+
+    for filename in glob.glob(assets_dir+"/*.js"):
+        with open(filename) as f:
+            data = f.read()
+            data = data.replace("192.168.0.11", api_url)
+            with open(assets_dir+"index.js", "w") as w:
+                w.write(data)
+
+if __name__ == "__main__":
+    argv = sys.argv
+    print(argv)
+    main(argv[1])
