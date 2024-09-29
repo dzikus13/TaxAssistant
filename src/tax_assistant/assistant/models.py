2039af77923c8db10fcbada8825ba996388a2fde
Piotr Kryska <piotr.kryska@yarrl.pl>
2024-09-29 03:58:35 +0200

    metadata

--- src/tax_assistant/assistant/models.py
+++ src/tax_assistant/assistant/models.py
@@ -1,18 +1,35 @@
 from django.db import models
 
+from django.utils.translation import gettext as _
 # Create your models here.
 
+SESSION_STATUS = {
+    "ongoing": _("Ongoing"),
+    "finished": _("Finished"),
+    "aborted": _("Aborted"),
+}
 
 class Session(models.Model):
     user_id = models.UUIDField()
     topic = models.CharField(max_length=100, null=True, blank=True)
-    created_at = models.DateTimeField(auto_created=True)
+    status = models.CharField(max_length=20,
+                               choices=[(k, v) for k, v in SESSION_STATUS.items()],
+                               default="ongoing")
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def collect_knowledge(self):
+        dataset = {}
+        for item in self.interaction_set.all():
+            for field in item.response_json.get("declaration_info", []):
+                dataset.update(field)
+        return dataset
 
 
 class Interaction(models.Model):
     session = models.ForeignKey(Session, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_created=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user_input = models.CharField(max_length=1024)
     response = models.TextField(null=True)
+    response_json = models.JSONField(null=True)
--- src/tax_assistant/assistant/views.py
+++ src/tax_assistant/assistant/views.py
@@ -5,10 +5,29 @@ from django.shortcuts import render
 from django.http.response import JsonResponse
 from rest_framework.decorators import api_view, action, permission_classes
 from .llm import OpenAIConnector, StopSessionException
-from .models import Session, Interaction
+from .models import Session, Interaction, SESSION_STATUS
 from xml_generator.models import TaxForm
 
 US_CODE_API_URL = "https://www.e-pity.pl/cms/inc/1/us/apiJson.php"
+
+SYSTEM_PROMPT = ""
+
+class HistoryDto:
+    def __init__(self, prompt, response):
+        self.prompt = prompt
+        self.response = response
+
+
+def combine_knowledge(metadata, values):
+    results = []
+    for item, value in metadata.items():
+        if item in values:
+            value["value"] = values[item]
+        results.append({
+            "field": item,
+            "value": value
+        })
+    return results
 
 
 @api_view(["POST"])
@@ -16,31 +35,50 @@ def process_input(request):
     data = json.loads(request.body)
     session_id = data["session_id"]
     prompt = data["prompt"]
-    session, created = Session.objects.get_or_create(id=session_id)
+    session, created = Session.objects.get_or_create(user_id=session_id)
     if created:
-        ai_connector = OpenAIConnector()  # TODO: system prompt
+        ai_connector = OpenAIConnector(SYSTEM_PROMPT)  # TODO: system prompt
         topic = ai_connector.discover_topic(prompt)
         session.topic = topic
         session.save()
     else:
         topic = session.topic
-    tax_form = TaxForm.objects.get(name=topic)
+
+    tax_form = TaxForm.objects.get(name=topic.upper())
     ai_connector = OpenAIConnector(tax_form.system_prompt)
-    interactions = session.interaction_set.all().order_by("created_at")
+    interactions = [ HistoryDto(item.user_input, item.response)
+                     for item in session.interaction_set.all().order_by("created_at")]
     try:
         ai_response = ai_connector.get_gpt_response(prompt, interactions)
+        datajson = json.loads(ai_response)
     except StopSessionException:
-        session.status = 
+        session.status = "aborted"
+        ai_response = "aborted"
+        datajson = {"status": "aborted"}
+        session.save()
+    
     interaction = Interaction(
         session=session,
         user_input=prompt,
-        respose=ai_response
+        response=ai_response,
+        response_json=datajson
     )
+    print(json.dumps(datajson, indent=2))
     interaction.save()
+    collected_data = combine_knowledge(tax_form.metadata, session.collect_knowledge())
+    if tax_form.is_ready(session.collect_knowledge()):
+        tax_form.create_document(session)
+        session.status = "finished"
+        session.save()
+    else:
+        missing = tax_form.get_missing_fields(session.collect_knowledge())
+        print("missinglist", missing)
+
     return JsonResponse({
         "status": "success",
-        "result": ai_response,
-        "session_status": session.get_status()
+        "result": datajson.get("answer"),
+        "session_status": session.status,
+        "session_collected": collected_data
     })
 
 
