ecbac92bb4d28cfbbddae09db693199aaba7355c
Piotr Kryska <piotr.kryska@yarrl.pl>
2024-09-29 09:40:22 +0200

    csrf

--- src/tax_assistant/assistant/urls.py
+++ src/tax_assistant/assistant/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('process_input/', views.process_input),
-    path('get_us_code/', views.get_us_code),
-    path('get_xml/', views.get_xml),
+    path('process_input/', csrf_exempt(views.process_input)),
+    path('get_us_code/', csrf_exempt(views.get_us_code)),
+    path('get_xml/', csrf_exempt(views.get_xml)),
 ]
 
